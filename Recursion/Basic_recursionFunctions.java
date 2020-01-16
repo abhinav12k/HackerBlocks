c5b716d7f12a8550c181c24eeb15dd40e2cef43a
Abhinav <abhi2000.rtk@gmail.com>
2020-01-17 00:30:12 +0530

    Basic recursion functions

--- Recursion/Basic_recursionFunctions.java
+++ Recursion/Basic_recursionFunctions.java
@@ -7,14 +7,30 @@ public class Basic_recursionFunctions {
 	public static void main(String[] args) {
 
 		Scanner scn = new Scanner(System.in);
-		int num = scn.nextInt();
+//		int num = scn.nextInt();
 
 		// strictly decreasing
 //		PD(num);
 		// strictly increasing
 //	PI(num);
 		// PDI with skips
-		PDI(num);
+//		PDI(num);
+
+		// factorial
+//		System.out.println(fact(num));
+//		System.out.println(power(2, 3));
+		// nth fibonacci number
+		System.out.println(fibonacci(2));
+
+	}
+
+	private static int fact(int num) {
+
+		if (num == 1) {
+			return num;
+		}
+
+		return num * fact(num - 1);
 
 	}
 
@@ -53,6 +69,27 @@ public class Basic_recursionFunctions {
 
 		}
 
+	}
+
+	public static long power(long x, long y) {
+
+		if (y == 0) {
+			return 1;
+		}
+
+		return x * power(x, y - 1);
+
+	}
+
+	public static int fibonacci(int n) {
+
+		if (n == 0 || n == 1) {
+			return n;
+		}
+
+		int a = fibonacci(n - 1);
+		int b = fibonacci(n - 2);
+		return a + b;
 	}
 
 }
