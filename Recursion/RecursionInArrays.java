db0e26cfd157f900dcbee66ace8a6bce8019e6cf
Abhinav <abhi2000.rtk@gmail.com>
2020-01-17 09:18:53 +0530

    Recursion Problem in Arrays

--- Recursion/RecursionInArrays.java
+++ Recursion/RecursionInArrays.java
@@ -16,6 +16,10 @@ public class RecursionInArrays {
 
 		System.out.println(max(arr, 0));
 		System.out.println(firstOccurenceIndex(arr, 0, 0));
+		System.out.println(lastOccurenceIndex1(arr, 0, 0));
+		for (int val : findAllIndex(arr, 0, 0, 0)) {
+			System.out.print(val + " ");
+		}
 
 	}
 
@@ -46,7 +50,59 @@ public class RecursionInArrays {
 
 		return firstOccurenceIndex(arr, item, vidx + 1);
 	}
-	
-	
+
+	// method1
+	public static int lastOccurenceIndex(int[] arr, int item, int vidx) {
+
+		if (vidx == arr.length) {
+			return -1;
+		}
+
+		int rr = lastOccurenceIndex(arr, item, vidx + 1);
+		if (item == arr[vidx] && rr == -1) {
+			return vidx;
+		}
+		return rr;
+	}
+
+	// method2
+	public static int lastOccurenceIndex1(int[] arr, int item, int vidx) {
+
+		if (vidx == arr.length) {
+			return -1;
+		}
+
+		int index = lastOccurenceIndex1(arr, item, vidx + 1);
+		if (index == -1) {
+
+			if (arr[vidx] == item)
+				return vidx;
+			else
+				return -1;
+
+		} else
+			return index;
+
+	}
+
+	// find all index
+	public static int[] findAllIndex(int[] arr, int item, int vidx, int count) {
+
+		if (vidx == arr.length) {
+			int[] narray = new int[count];
+			return narray;
+		}
+
+		int[] rr;
+		if (arr[vidx] == item) {
+
+			rr = findAllIndex(arr, item, vidx + 1, count + 1);
+			rr[count] = vidx;
+		} else {
+			rr = findAllIndex(arr, item, vidx + 1, count);
+		}
+
+		return rr;
+	}
 
 }
