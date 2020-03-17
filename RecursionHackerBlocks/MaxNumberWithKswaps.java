9d56aa00e3d8a7598cb2d643bfabbb48498ca2e3
Abhinav <abhi2000.rtk@gmail.com>
2020-03-18 01:48:25 +0530

    Updated

--- RecursionHackerBlocks/MaxNumberWithKswaps.java
+++ RecursionHackerBlocks/MaxNumberWithKswaps.java
@@ -22,7 +22,7 @@ public class MaxNumberWithKswaps {
 			return max;
 		}
 		
-		for(int i=0;i<number.length;i++) {
+		for(int i=vidx;i<number.length;i++) {
 			for(int j=i+1;j<number.length;j++) {
 				
 				if(number[i]<number[j]) {
