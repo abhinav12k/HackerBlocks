7d08ba5c27e364d4947cd111951cf8a4b11ff945
Abhinav <abhi2000.rtk@gmail.com>
2020-04-20 19:59:54 +0530

    Updated several codes

---
+++ RecursionHackerBlocks/SudokuSolver.java
@@ -0,0 +1,14 @@
+package HackerBlocks.RecursionHackerBlocks;
+
+import java.util.Scanner;
+
+public class SudokuSolver {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		int size = scn.nextInt();
+		
+	}
+	
+}
